df5b8b177397708aefa8a6233f884e8eb8de8e5b
brn <brn8@njit.edu>
2021-09-07 23:34:29 -0400

    exercise 1

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// function getButtonText(){
+// 	return 'Click on me!';
+// }
 //Create a react component
+
+function getTime(){
+	let date = new Date();
+	return (new Date().toLocaleTimeString());
+}
 const App = () =>{
-	return <div>Hi there!</div>;
+	const buttonText = { text: 'Click me'};
+	let time = "Current Time:  "
+	return (
+		<div>
+		<label>{time}{getTime()}</label><br/><br/>
+		<label className="label" htmlFor="name">Enter Name:</label>
+		<input id="name" type="text"/>
+  		<button style={{backgroundColor:'blue', color:'white'}}>{buttonText.text}</button>
+		</div>
+);
  };
 
 ReactDOM.render(<App/>,document.getElementById("root"));
