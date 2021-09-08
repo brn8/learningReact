5d9a82c5d14d02edd08b21d3efe67a98fd74870a
brn <brn8@njit.edu>
2021-09-07 22:19:33 -0400

    learning react

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+//Create a react component
+const App = () =>{
+	return <div>Hi there!</div>;
+ };
+
+ReactDOM.render(<App/>,document.getElementById("root"));
+
+
+
